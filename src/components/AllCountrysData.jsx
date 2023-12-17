d3e394c46b643587bdd377e89dab35bbc8b1fc53
ArafathShemanto <arafathh.shemanto@gmail.com>
2023-12-18 01:06:19 +0600

    add

--- src/components/AllCountrysData.jsx
+++ src/components/AllCountrysData.jsx
@@ -1,36 +1,53 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 
 const AllCountryList = () => {
-    const [contacts, setContacts] = useState([]);
-    console.log(contacts)
-    useEffect(() => {
-        fetchData();
-    }, []);
+  const [contacts, setContacts] = useState([]);
+  const [showEvenIds, setShowEvenIds] = useState(false);
 
-    const fetchData = () => {
-        fetch('https://contact.mediusware.com/api/contacts/')
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error('Network response was not ok');
-                }
-                return response.json();
-            })
-            .then(data => setContacts(data?.results))
-            .catch(error => console.error('Error fetching data:', error));
-    };
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-    return (
-        <div>
-            <h2>All Country List Here</h2>
-            <ul className='max-h-500'>
-                {contacts.map((contact, index) => (
-                    <li key={index}>
-                       {contact?.country?.name}
-                    </li>
-                ))}
-            </ul>
-        </div>
-    );
+  const fetchData = () => {
+    fetch("https://contact.mediusware.com/api/contacts/")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then((data) => setContacts(data?.results))
+      .catch((error) => console.error("Error fetching data:", error));
+  };
+
+  const handleCheckboxChange = () => {
+    setShowEvenIds(!showEvenIds);
+  };
+
+  return (
+    <div>
+      <h2>All Country</h2>
+
+      <ul className="max-h-500">
+        {contacts.map((contact, index) =>
+          (showEvenIds && index % 2 === 0) || !showEvenIds ? (
+            <li key={index}>{contact?.country?.name}</li>
+          ) : null
+        )}
+      </ul>
+      <div className="mt-3">
+        <label>
+          <input
+            type="checkbox"
+            checked={showEvenIds}
+            className="mr-3"
+            onChange={handleCheckboxChange}
+          />
+          Show Even IDs
+        </label>
+      </div>
+    </div>
+  );
 };
 
 export default AllCountryList;
--- src/components/UsaData.jsx
+++ src/components/UsaData.jsx
@@ -1,36 +1,54 @@
 import React, { useEffect, useState } from 'react';
 
 const USAList = () => {
-    const [contacts, setContacts] = useState([]);
-    console.log(contacts)
-    useEffect(() => {
-        fetchData();
-    }, []);
+  const [contacts, setContacts] = useState([]);
+  const [showEvenIds, setShowEvenIds] = useState(false);
 
-    const fetchData = () => {
-        fetch('https://contact.mediusware.com/api/country-contacts/United%20States/?page=1')
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error('Network response was not ok');
-                }
-                return response.json();
-            })
-            .then(data => setContacts(data?.results))
-            .catch(error => console.error('Error fetching data:', error));
-    };
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-    return (
-        <div>
-            <h2>All Country List Here</h2>
-            <ul className='max-h-500'>
-                {contacts.map((contact, index) => (
-                    <li key={index}>
-                       {contact?.country?.name}
-                    </li>
-                ))}
-            </ul>
-        </div>
-    );
+  const fetchData = () => {
+    fetch('https://contact.mediusware.com/api/country-contacts/United%20States/?page=1')
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then(data => setContacts(data?.results))
+      .catch(error => console.error('Error fetching data:', error));
+  };
+
+  const handleCheckboxChange = () => {
+    setShowEvenIds(!showEvenIds);
+  };
+
+  return (
+    <div>
+      <h2>USA ONLY</h2>
+     
+      <ul className='max-h-500'>
+        {contacts.map((contact, index) => (
+          (showEvenIds && index % 2 === 0) || !showEvenIds ? (
+            <li key={index}>
+              {contact?.country?.name}
+            </li>
+          ) : null
+        ))}
+      </ul>
+      <div>
+        <label>
+          <input
+            type="checkbox"
+            checked={showEvenIds}
+            onChange={handleCheckboxChange}
+          />
+          Show Even IDs
+        </label>
+      </div>
+    </div>
+  );
 };
 
 export default USAList;
